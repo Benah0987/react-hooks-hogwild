dd4fd36bc3a68226e0e4942138b0977b5f8494d2
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-08 09:14:41 +0300

    creating a filter component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import piggy from "../assets/porco.png";
 
 //passing down props
-const Nav = ({}) => {
+const Nav = ({handleChange, handleGreased}) => {
 	return (
 		<div className="navWrapper">
 			<span className="headerText">HogWild</span>
@@ -12,6 +12,9 @@ const Nav = ({}) => {
 			<span className="normalText">
 				A React App for County Fair Hog Fans
 			</span>
+			<button onClick = {handleGreased} >Grease</button>
+			{/*passnig down prop to Filter child*/}
+			<Filter 
 		</div>
 	);
 };
